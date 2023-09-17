4e327f0326dfd511498e3ee853b40275cf311f7b
Abimbola Okusada <44692035+abimbola001@users.noreply.github.com>
2023-09-17 12:34:30 +0300

    Create terraform_code.tf

---
+++ terraform_code.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.0.13"
+    }
+  }
+}
+
+
+provider "kind" {
+  #name = "nodes-test"
+}
+
+provider "kubernetes" {
+  config_path = "/root/.kube/config"
+}
+provider "helm" {
+  kubernetes {
+    config_path = "<your_kubeconfig_path>"
+  }
+}
+
+resource "kubernetes_secret" "docker-registry" {
+  metadata {
+    name = "dockerhubpull"
+  }
+}
+resource "kubernetes_deployment" "Nodeappdeployment" {
+  metadata {
+    name = "nodeapp1"
+    #namespace = "hello_world"
+    labels = {
+      app = "nodejs"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "nodejs"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "nodejs"
+        }
+      }
+      spec {
+        container {
+          image = "aokusada/hello1-express"
+          name  = "hello-express"
+
+
+        }
+      }
+    }
+  }
