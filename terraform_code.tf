e7de7798d9829a402fd8d942ad0df29eb413f8ec
Abimbola Okusada <44692035+abimbola001@users.noreply.github.com>
2023-09-20 08:46:09 +0300

    Update terraform_code.tf

--- terraform_code.tf
+++ terraform_code.tf
@@ -4,10 +4,19 @@ terraform {
       source = "tehcyx/kind"
       version = "0.0.13"
     }
+
+   kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
 
-
+provider "helm" {
+  kubernetes {
+    config_path = "/root/.kube/config"
+  }
+}
 provider "kind" {
   #name = "nodes-test"
 }
@@ -15,71 +24,11 @@ provider "kind" {
 provider "kubernetes" {
   config_path = "/root/.kube/config"
 }
-provider "helm" {
-  kubernetes {
-    config_path = "<your_kubeconfig_path>"
-  }
+
+resource "kubectl_manifest" "nodejs" {
+  yaml_body = file("${path.module}/deployment.yaml")
 }
 
-resource "kubernetes_secret" "docker-registry" {
-  metadata {
-    name = "dockerhubpull"
-  }
+provider "kubectl" {
+  config_path = "/root/.kube/config"  # Path to your kubeconfig file
 }
-resource "kubernetes_deployment" "Nodeappdeployment" {
-  metadata {
-    name = "nodeapp1"
-    #namespace = "hello_world"
-    labels = {
-      app = "nodejs"
-    }
-  }
-
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "nodejs"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "nodejs"
-        }
-      }
-      spec {
-        container {
-          image = "aokusada/hello1-express"
-          name  = "hello-express"
-
-
-        }
-      }
-    }
-  }
-
-resource "kubernetes_service" "hello_world1_service" {
-  depends_on = [kubernetes_deployment.Nodeappdeployment]
-
-  metadata {
-    labels = {
-      app = "hello-world-example1"
-    }
-    name = "hello-world-example1"
-    #namespace = kubernetes_namespace.hello_world_namespace.metadata.0.name
-  }
-
-  spec {
-    port {
-      name = "api1"
-      port = 8080
-      target_port = 8080
-    }
-    selector = {
-      app = "hello-world-example1"
-    }
-    type = "NodePort"
-  }
-}
-
